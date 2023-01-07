17797d0e25406740df28a796cbd9feb375603a28
Artem LAPOV <artemlapov@me.com>
2023-01-07 16:33:16 +0100

    Initial Commit

---
+++ Magasinage/MagasinageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagasinageApp.swift
+//  Magasinage
+//
+//  Created by Artem Lapov on 07.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MagasinageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
